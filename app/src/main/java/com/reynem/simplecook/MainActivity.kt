bdb76c551d5bffb35bb43210a94654fb71d19878
Kerim_Lex <lexmeins1@gmail.com>
2025-07-11 13:42:14 +0500

    Just learning how to use Images

--- app/src/main/java/com/reynem/simplecook/MainActivity.kt
+++ app/src/main/java/com/reynem/simplecook/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.reynem.simplecook.ui.theme.SimpleCookTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimpleCookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp(Modifier.fillMaxSize())
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier){
+    SimpleCookTheme{
+        Scaffold(modifier = modifier,
+            containerColor = MaterialTheme.colorScheme.background) { innerPadding ->
+            Column{
+                ProfileContainer(modifier = Modifier.padding(innerPadding))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ProfileContainer(modifier: Modifier = Modifier){
+    val image = R.drawable.profile_picture
+    val imageModifier = modifier
+        .size(120.dp)
+        .padding(10.dp)
+        .clip(CircleShape)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleCookTheme {
-        Greeting("Android")
-    }
+
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = "Profile Picture",
+        modifier = imageModifier,
+    )
 }
