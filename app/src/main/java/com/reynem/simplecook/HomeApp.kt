8bde56a50a3c37a1d29918d5440ccf347fb1f4f7
Kerim_Lex <lexmeins1@gmail.com>
2025-07-27 14:54:45 +0500

    Make home screen scrollable
    - Change home screen from Scrollable to LazyColumn.
    - Update Image composable from AsyncImage to SubcomposeAsyncImage to prevent crashes.
    - Minor design changes.

--- app/src/main/java/com/reynem/simplecook/HomeApp.kt
+++ app/src/main/java/com/reynem/simplecook/HomeApp.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,12 +25,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.getString
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
 import com.reynem.simplecook.api.RecipeViewModel
 import com.reynem.simplecook.api.models.Recipe
 import com.reynem.simplecook.ui.theme.SimpleCookTheme
@@ -43,36 +42,46 @@ fun HomeApp(modifier: Modifier = Modifier, recipeViewModel: RecipeViewModel = vi
 
     Column(
         modifier = modifier
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize(),
+            .padding(16.dp)
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         OutlinedTextField(
             value = ingredients,
             onValueChange = { ingredients = it },
             label = { Text("Ingredients") },
-            modifier = Modifier
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = {
-            recipeViewModel.fetchRecipes(ingredients)
-        }) {
+                recipeViewModel.fetchRecipes(ingredients)
+            }) {
             Text("Search")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         if (error.isNotEmpty()) {
-            Text(text = error)
+            Text(
+                text = error,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(16.dp)
+            )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        recipes.forEach { recipe ->
-            RecipeItem(recipe = recipe)
+
+        LazyColumn(
+            modifier = modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(recipes) { recipe ->
+                RecipeItem(recipe = recipe)
+            }
         }
     }
 }
@@ -82,16 +91,18 @@ fun RecipeItem(recipe: Recipe) {
     Row (
         modifier = Modifier
             .padding(16.dp)
-            .background(color = MaterialTheme.colorScheme.secondaryContainer )
+            .background(color = MaterialTheme.colorScheme.secondaryContainer)
             .padding(vertical = 16.dp)
     ){
         Box(
-            modifier = Modifier.height(240.dp).padding(horizontal = 8.dp)
+            modifier = Modifier
+                .height(240.dp)
+                .padding(horizontal = 8.dp)
         ) {
-            AsyncImage(
+            SubcomposeAsyncImage(
                 model = recipe.image,
                 contentDescription = "Recipe image " + recipe.id,
-                contentScale = ContentScale.Fit
+                contentScale = ContentScale.Crop,
             )
         }
         Column(
@@ -99,9 +110,7 @@ fun RecipeItem(recipe: Recipe) {
         ){
             Text(text = recipe.title)
             Text(
-                text = String.format(getString(LocalContext.current,
-                    R.string.number_of_ingredients),
-                    recipe.usedIngredientCount),
+                text = stringResource(R.string.number_of_ingredients, recipe.usedIngredientCount),
                 color = Color.Gray,
                 fontSize = MaterialTheme.typography.bodySmall.fontSize
             )
