64d036d159f0cb7b5bbe76e0c22ff381517687e1
Kerim_Lex <lexmeins1@gmail.com>
2025-07-30 16:39:26 +0500

    Added Ingredients FlowRow
    - Created a FlowRow to display ingredients.
    - Added padding to the FlowRow.
    - Iterated through the ingredients list to create an IngredientButton for each.
    - Updated IngredientButton to accept padding and font size parameters.
    - Created a Preview for the IngredientButton.

--- app/src/main/java/com/reynem/simplecook/CompoundApp.kt
+++ app/src/main/java/com/reynem/simplecook/CompoundApp.kt
@@ -1,8 +1,7 @@
 package com.reynem.simplecook
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -13,13 +12,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+val ingredients : List<String> = listOf("Tomato", "Garlic", "Potato")
 
 @Composable
 fun CompoundApp(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()){
-        IngredientButton("Tomato")
+    FlowRow (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 10.dp)
+    ){
+        repeat(ingredients.size){ ingredient ->
+            IngredientButton(ingredients[ingredient])
+        }
     }
 }
 
@@ -27,6 +35,7 @@ fun CompoundApp(modifier: Modifier = Modifier) {
 fun IngredientButton(ingredient: String){
     var flag by remember { mutableStateOf(false) }
     Button(
+        contentPadding = PaddingValues(vertical = 4.dp, horizontal = 12.dp),
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.inversePrimary),
         shape = MaterialTheme.shapes.large,
         colors = ButtonDefaults.buttonColors(containerColor =
@@ -41,7 +50,14 @@ fun IngredientButton(ingredient: String){
         Text(
             color = if (flag) {MaterialTheme.colorScheme.onSurface}
                     else {MaterialTheme.colorScheme.onPrimary},
+            fontSize = 12f.sp,
             text = ingredient
         )
     }
+}
+
+@Preview
+@Composable
+fun IngredientButtonPreview(){
+    CompoundApp()
 }
